1edf97356e6bca927bc7ea6d095faf704286d9ee
y_kumagai <y.kuma1102@gmail.com>
2025-05-10 06:46:41 +0000

    example_random_greeting

---
+++ example/random_greeting/greetings/greetings.go
@@ -0,0 +1,28 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
